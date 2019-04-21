3b8c24f88b7600b8b5a584daa123bf5fe7344dea
kohei-okazaki <kou1210hei@gmail.com>
2019-04-21 22:14:55 +0900

    no message

--- kin-dashboard/src/main/java/jp/co/kin/dashboard/controller/LoginController.java
+++ kin-dashboard/src/main/java/jp/co/kin/dashboard/controller/LoginController.java
@@ -1,4 +1,4 @@
-package jp.co.kin.controller;
+package jp.co.kin.dashboard.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
