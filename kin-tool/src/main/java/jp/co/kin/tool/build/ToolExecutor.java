a5d43f6b3e1fd3e72b6d5a89b3a5ef4db5793084
kou1210hei <kou1210hei@gmail.com>
2019-06-09 15:32:36 +0900

    no message

--- kin-tool/src/main/java/jp/co/kin/tool/build/ToolExecutor.java
+++ kin-tool/src/main/java/jp/co/kin/tool/build/ToolExecutor.java
@@ -5,14 +5,17 @@ public class ToolExecutor {
 	/**
 	 * 実行したいビルダークラスをjp.co.kin.tool.build.BuildInvoker#buildの引数に設定して下さい
 	 * <ul>
-	 * <li>EntityBuilder</li>
+	 * <li>AddColumnBuilder</li>
 	 * <li>CreateTableBuilder</li>
+	 * <li>DropBuilder</li>
+	 * <li>EntityBuilder</li>
+	 * <li>TableDefineBuilder</li>
 	 * </ul>
 	 *
 	 * @param args
 	 *            使わない
 	 */
 	public static void main(String[] args) {
-		BuildInvoker.build("CreateTableBuilder", "EntityBuilder");
+		BuildInvoker.build("CreateTableBuilder", "DropBuilder", "EntityBuilder");
 	}
 }
