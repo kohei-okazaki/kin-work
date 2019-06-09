9717793f233f679117fb4a33c4e3ac93bcd367a6
kou1210hei <kou1210hei@gmail.com>
2019-06-09 14:24:45 +0900

    no message

--- kin-dashboard/src/main/java/jp/co/kin/dashboard/login/controller/LoginController.java
+++ kin-dashboard/src/main/java/jp/co/kin/dashboard/login/controller/LoginController.java
@@ -1,7 +1,11 @@
 package jp.co.kin.dashboard.login.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.kin.dashboard.login.form.LoginForm;
 import jp.co.kin.dashboard.type.DashboardView;
 import jp.co.kin.web.controller.BaseViewController;
 
@@ -9,8 +13,20 @@ import jp.co.kin.web.controller.BaseViewController;
  * ログインコントローラ
  */
 @Controller
+@RequestMapping("login")
 public class LoginController implements BaseViewController {
 
+	/**
+	 * Formを返す
+	 *
+	 * @return LoginForm
+	 */
+	@ModelAttribute("loginForm")
+	public LoginForm setUpForm() {
+		return new LoginForm();
+	}
+
+	@GetMapping("/index")
 	public String index() {
 		return getView(DashboardView.LOGIN);
 	}
