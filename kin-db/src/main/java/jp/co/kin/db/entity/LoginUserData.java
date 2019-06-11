14cd88eb5a07cfaf29077d4c027323eb68bbcb00
kou1210hei <kou1210hei@gmail.com>
2019-06-11 21:33:18 +0900

    no message

---
+++ kin-db/src/main/java/jp/co/kin/db/entity/LoginUserData.java
@@ -0,0 +1,82 @@
+package jp.co.kin.db.entity;
+
+import java.util.Date;
+
+import org.seasar.doma.Entity;
+import org.seasar.doma.jdbc.entity.NamingType;
+
+@Entity(naming = NamingType.SNAKE_UPPER_CASE)
+public class LoginUserData implements BaseEntity {
+
+	/** ログインID */
+	private String loginId;
+	/** ユーザ名 */
+	private String userName;
+	/** パスワード */
+	private String password;
+	/** パスワード有効期限 */
+	private Date passwordExpire;
+	/** ユーザ権限 */
+	private String userAuth;
+	/** 更新日時 */
+	private Date updateDate;
+	/** 登録日時 */
+	private Date regDate;
+
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+
+	public String getLoginId() {
+		return loginId;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPasswordExpire(Date passwordExpire) {
+		this.passwordExpire = passwordExpire;
+	}
+
+	public Date getPasswordExpire() {
+		return passwordExpire;
+	}
+
+	public void setUserAuth(String userAuth) {
+		this.userAuth = userAuth;
+	}
+
+	public String getUserAuth() {
+		return userAuth;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+}
