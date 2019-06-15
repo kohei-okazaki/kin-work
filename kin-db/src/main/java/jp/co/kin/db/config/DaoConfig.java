18f8f84f35179c52eedb7aceb5ad8474a1686a73
kou1210hei <kou1210hei@gmail.com>
2019-06-15 13:51:08 +0900

    no message

--- kin-db/src/main/java/jp/co/kin/db/config/DaoConfig.java
+++ kin-db/src/main/java/jp/co/kin/db/config/DaoConfig.java
@@ -1,12 +1,20 @@
 package jp.co.kin.db.config;
 
+import java.util.function.Supplier;
+
 import javax.sql.DataSource;
 
+import org.seasar.doma.jdbc.AbstractJdbcLogger;
 import org.seasar.doma.jdbc.Config;
+import org.seasar.doma.jdbc.JdbcLogger;
 import org.seasar.doma.jdbc.SqlFileRepository;
 import org.seasar.doma.jdbc.dialect.Dialect;
+import org.slf4j.event.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+
+import jp.co.kin.common.log.Logger;
+import jp.co.kin.common.log.LoggerFactory;
 
 /**
  * Dao設定情報クラス
@@ -49,6 +57,58 @@ public class DaoConfig implements Config {
 
 	public void setSqlFileRepository(SqlFileRepository sqlFileRepository) {
 		this.sqlFileRepository = sqlFileRepository;
+	}
+
+	@Override
+	public JdbcLogger getJdbcLogger() {
+		return new DaoLogger(Level.INFO);
+	}
+
+	/**
+	 * Daoのロガークラス
+	 *
+	 */
+	public static class DaoLogger extends AbstractJdbcLogger<Level> {
+
+		private static final Logger LOG = LoggerFactory.getLogger(DaoLogger.class);
+
+		public DaoLogger() {
+			this(Level.DEBUG);
+		}
+
+		public DaoLogger(Level level) {
+			super(level);
+		}
+
+		@Override
+		public void log(Level level, String callerClassName, String callerMethodName, Throwable throwable,
+				Supplier<String> messageSupplier) {
+
+			switch (level) {
+			case ERROR:
+				LOG.error(buildLogMessage(callerClassName, callerMethodName, messageSupplier), throwable);
+				break;
+			case WARN:
+				LOG.warn(buildLogMessage(callerClassName, callerMethodName, messageSupplier), throwable);
+				break;
+			case INFO:
+				LOG.info(buildLogMessage(callerClassName, callerMethodName, messageSupplier));
+				break;
+			case DEBUG:
+				LOG.debug(buildLogMessage(callerClassName, callerMethodName, messageSupplier));
+				break;
+			default:
+				LOG.trace(buildLogMessage(callerClassName, callerMethodName, messageSupplier));
+				break;
+			}
+
+		}
+
+		private String buildLogMessage(String callerClassName, String callerMethodName,
+				Supplier<String> messageSupplier) {
+			return "| - " + callerClassName + ", " + messageSupplier.get();
+		}
+
 	}
 
 }
