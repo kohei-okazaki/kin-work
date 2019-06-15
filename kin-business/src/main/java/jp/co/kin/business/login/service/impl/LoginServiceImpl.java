0c15b0337a3a8d30fa88c99da681e20dd6261833
kou1210hei <kou1210hei@gmail.com>
2019-06-15 21:42:12 +0900

    no message

--- kin-business/src/main/java/jp/co/kin/business/login/service/impl/LoginServiceImpl.java
+++ kin-business/src/main/java/jp/co/kin/business/login/service/impl/LoginServiceImpl.java
@@ -1,11 +1,16 @@
 package jp.co.kin.business.login.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import jp.co.kin.business.login.service.LoginService;
+import jp.co.kin.db.dao.LoginUserDataDao;
 
 @Service
 public class LoginServiceImpl implements LoginService {
+
+	@Autowired
+	private LoginUserDataDao dao;
 
 	@Override
 	public String check(String name) {
--- kin-db/src/main/java/jp/co/kin/db/dao/LoginUserDataDao.java
+++ kin-db/src/main/java/jp/co/kin/db/dao/LoginUserDataDao.java
@@ -3,6 +3,7 @@ package jp.co.kin.db.dao;
 import org.seasar.doma.Dao;
 import org.seasar.doma.Delete;
 import org.seasar.doma.Insert;
+import org.seasar.doma.Select;
 import org.seasar.doma.Update;
 
 import jp.co.kin.db.config.DaoRepository;
@@ -20,5 +21,8 @@ public interface LoginUserDataDao extends BaseDao {
 
 	@Insert
 	public int insert(LoginUserData entity);
+
+	@Select
+	public LoginUserData selectById(String loginId);
 
 }
