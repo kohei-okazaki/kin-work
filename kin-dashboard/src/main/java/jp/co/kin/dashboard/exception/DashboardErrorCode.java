78c7fc7e6d5136dde2140d46a5cda6f2e6794907
kou1210hei <kou1210hei@gmail.com>
2019-07-07 17:31:27 +0900

    no message

--- kin-dashboard/src/main/java/jp/co/kin/dashboard/exception/DashboardErrorCode.java
+++ kin-dashboard/src/main/java/jp/co/kin/dashboard/exception/DashboardErrorCode.java
@@ -8,15 +8,15 @@ public enum DashboardErrorCode implements BaseErrorCode {
 	/** 不正なリクエストエラー */
 	ILLEGAL_REQUEST("WD0003", LogLevel.WARN);
 
-	private DashboardErrorCode(String outerErrorCode, LogLevel logLevel) {
-		this.outerErrorCode = outerErrorCode;
-		this.logLevel = logLevel;
-	}
-
 	/** 外部用エラーコード */
 	private String outerErrorCode;
 	/** ログレベル */
 	private LogLevel logLevel;
+
+	private DashboardErrorCode(String outerErrorCode, LogLevel logLevel) {
+		this.outerErrorCode = outerErrorCode;
+		this.logLevel = logLevel;
+	}
 
 	@Override
 	public String getOuterErrorCode() {
