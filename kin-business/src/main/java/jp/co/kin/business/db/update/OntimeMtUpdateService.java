5f03ac74e2b9ead929ae6564d0e24888200ffa05
kou1210hei <kou1210hei@gmail.com>
2019-07-24 17:21:31 +0900

    no message

--- kin-business/src/main/java/jp/co/kin/business/db/update/OntimeMtUpdateService.java
+++ kin-business/src/main/java/jp/co/kin/business/db/update/OntimeMtUpdateService.java
@@ -5,5 +5,11 @@ import jp.co.kin.web.service.BaseService;
 
 public interface OntimeMtUpdateService extends BaseService {
 
+	/**
+	 * 定時情報マスタを更新する
+	 * 
+	 * @param entity
+	 *            OntimeMt
+	 */
 	void update(OntimeMt entity);
 }
