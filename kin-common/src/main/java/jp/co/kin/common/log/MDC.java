59c493395be011053b7229e3cb90c877c3fab977
kou1210hei <kou1210hei@gmail.com>
2019-09-26 21:20:11 +0900

    no message

--- kin-common/src/main/java/jp/co/kin/common/log/MDC.java
+++ kin-common/src/main/java/jp/co/kin/common/log/MDC.java
@@ -2,6 +2,8 @@ package jp.co.kin.common.log;
 
 import java.util.Optional;
 
+import jp.co.kin.common.util.StringUtil;
+
 /**
  * Logの追加情報を管理するクラス
  *
@@ -17,5 +19,13 @@ public class MDC {
 
 	public static String get(String key) {
 		return Optional.ofNullable(org.slf4j.MDC.get(key)).orElse("<NULL>");
+	}
+
+	public static void remove(String key) {
+		org.slf4j.MDC.remove(key);
+	}
+
+	public static boolean isContains(String key) {
+		return StringUtil.hasValue(get(key));
 	}
 }
