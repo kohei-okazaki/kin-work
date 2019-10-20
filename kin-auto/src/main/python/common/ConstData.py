dc8f6bf27ed63d30786e07f6d28c8aeb335b6d04
kou1210hei <kou1210hei@gmail.com>
2019-10-20 18:41:43 +0900

    no message

---
+++ kin-auto/src/main/python/common/ConstData.py
@@ -0,0 +1,11 @@
+'''
+Created on 2019/10/20
+共通情報の定義情報
+
+@since 1.0.0
+
+'''
+# Driverパス
+SELENIUM_DRIVER_PATH = 'C:\kin-app/git/kin-work/kin-auto/bin/chromedriver.exe'
+# RequestベースURL
+BASE_REQUEST_URL = 'http://localhost:8080/kin-dashboard'
