45bbdbbb5ee72dda82e99bd81b385f1732f5e55a
kou1210hei <kou1210hei@gmail.com>
2019-11-04 16:37:07 +0900

    no message

--- kin-auto/src/main/python/attendregist/AttendTest.py
+++ kin-auto/src/main/python/attendregist/AttendTest.py
@@ -9,13 +9,16 @@ Created on 2019/10/27
 from time import sleep
 from selenium import webdriver
 from src.main.python.common import ConstData
+from src.main.python.common import Logger
+
+log = Logger.Logger("auto.log")
+log.write("AttendTest開始")
 
 # Driverパスを設定する
 driver = webdriver.Chrome(ConstData.SELENIUM_DRIVER_PATH)
 
 # ログインを行う
 driver.get(ConstData.BASE_REQUEST_URL + '/login/index')
-print("ログイン画面のURL=" + driver.current_url)
 
 # ログイン画面 - loginIdにtestを設定
 loginIdElement = driver.find_element_by_id("loginId")
@@ -34,8 +37,7 @@ submiyButton.click()
 # 読み込みを待つために2秒間処理を止める
 sleep(2)
 
-print("TOP画面のURL=" + driver.current_url)
-
 # 勤怠登録画面へ遷移
 driver.find_element_by_link_text(u"勤怠登録").click()
 
+log.write("AttendTest修正")
