a4b39119d598192d2a1664d6415d515f0c15c980
kohei.okazaki <kou1210hei@gmail.com>
2020-01-18 15:03:09 +0900

    no message

--- kin-auto/src/main/python/attendregist/AttendTest.py
+++ kin-auto/src/main/python/attendregist/AttendTest.py
@@ -6,6 +6,7 @@ Created on 2019/10/27
 @since 1.0.0
 
 '''
+import datetime
 from time import sleep
 
 from selenium import webdriver
@@ -14,6 +15,27 @@ from selenium.webdriver.support.ui import Select
 from src.main.python.common import ConstData
 from src.main.python.common import Logger
 from src.main.python.common import LoginAuth
+
+def testSelectList(driver):
+
+    # システム日時を取得
+    sysdate = datetime.date.today()
+    sysdateYear = sysdate.year
+    # selectボックス-年を変更する(先頭から最後まで)
+    yearEelement = driver.find_element_by_name('year')
+    yearEelementSelected = Select(yearEelement)
+    for i in range(0, 9):
+        sleep(1)
+        yearEelementSelected.select_by_value(str(sysdateYear + i))
+
+def testMonthList(driver):
+    # selectボックス-月を変更する(先頭から最後まで)
+    monthEelement = driver.find_element_by_name('month')
+    monthList = [1, 2, 3, 4, 5, 6, 7, 8 , 9, 10, 11, 12]
+    monthEelementSelected = Select(monthEelement)
+    for month in monthList:
+        sleep(1)
+        monthEelementSelected.select_by_value(str(month))
 
 ''' ここからメイン処理 '''
 log = Logger.Logger("auto.log")
@@ -31,13 +53,11 @@ result = loginAuth.login()
 # 勤怠登録画面へ遷移
 driver.find_element_by_link_text(u"勤怠登録").click()
 
-# selectボックス-年を変更する(初期値->2021->2022)
-yearEelement = driver.find_element_by_name('year')
-yearEelementSelected = Select(yearEelement)
-yearEelementSelected.select_by_value('2021')
-sleep(1)
-yearEelementSelected.select_by_value('2022')
-sleep(1)
+# セレクトリスト(年)のテストを行う
+testSelectList(driver)
+
+# セレクトリスト(月)のテストを行う
+testMonthList(driver)
 
 log.write("AttendTest終了")
 
